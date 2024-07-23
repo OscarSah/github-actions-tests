ec9cffe669c7f780942e75383f31b275f3f5202c
OscarSah <oscar@cybertekschool.com>
2024-07-23 16:22:47 -0400

    create tests

---
+++ src/test/java/tests/Test01.java
@@ -0,0 +1,11 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Test01 {
+    @Test
+    public void test01(){
+        Assertions.assertEquals(5,5);
+    }
+}
